d82382b77a538de7e10e07b7d6313a7bd8f04ffe
Aref Azizian <storm.aref@outlook.com>
2019-07-17 23:05:37 +0430

    login added

--- app/src/main/java/org/arshef/banafsh/views/LoginActivity.java
+++ app/src/main/java/org/arshef/banafsh/views/LoginActivity.java
@@ -2,6 +2,8 @@ package org.arshef.banafsh.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.arshef.banafsh.R;
 
@@ -11,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
