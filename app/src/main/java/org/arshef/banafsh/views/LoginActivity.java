7039258621dfe1dddf94fda71195f73dfb67695b
Aref Azizian <storm.aref@outlook.com>
2019-07-31 11:01:28 +0430

    progress

--- app/src/main/java/org/arshef/banafsh/views/LoginActivity.java
+++ app/src/main/java/org/arshef/banafsh/views/LoginActivity.java
@@ -1,7 +1,11 @@
 package org.arshef.banafsh.views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +15,13 @@ import android.widget.Toast;
 import org.arshef.banafsh.R;
 
 public class LoginActivity extends AppCompatActivity {
+    final String Username = "a";
+    final String Password = "a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        permission();
         setContentView(R.layout.activity_login);
         Button loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText username = findViewById(R.id.usrTxt);
                 EditText password = findViewById(R.id.passTxt);
-                if (username.getText().toString().equals("a") && password.getText().toString().equals("a"))
+                if (username.getText().toString().equals(Username) && password.getText().toString().equals(Password))
                     Login();
                 else
                     Toast.makeText(LoginActivity.this, "ورود ممکن نیست", Toast.LENGTH_SHORT).show();
@@ -30,8 +37,30 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(LoginActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+        }
+    }
+
     private void Login() {
         Toast.makeText(LoginActivity.this, "خوش آمدید", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(LoginActivity.this, ChooseActivity.class));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(LoginActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                System.exit(0);
+            }
+        }
     }
 }
